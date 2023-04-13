ec060f3c04addbfc404a66085aa75a03b785cd31
Shane Dorsey <scdorsey22@gmail.com>
2023-04-13 15:11:51 -0700

    building MVC

--- vanilla/js/app.js
+++ vanilla/js/app.js
@@ -1,59 +1,174 @@
-const App = {
-    
-    //All of our selected HTML elements
-    $: {
-        menu: document.querySelector('[data-id="menu"]'),
-        menuItems: document.querySelector('[data-id="menu-popover"]'),
-        resetBtn: document.querySelector('[data-id="reset-btn"]'),
-        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
-        squares: document.querySelectorAll('[data-id="square"]')
-        
-    },
+import View from './view.js'
 
-    state: {
-        currentPlayer: 1
-    },
+// const App = {
+//   //All of our selected HTML elements
+//   $: {
+//     menu: document.querySelector('[data-id="menu"]'),
+//     menuItems: document.querySelector('[data-id="menu-popover"]'),
+//     resetBtn: document.querySelector('[data-id="reset-btn"]'),
+//     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+//     squares: document.querySelectorAll('[data-id="square"]'),
+//     modal: document.querySelector('[data-id="modal"]'),
+//     modalText: document.querySelector('[data-id="modal-text"]'),
+//     modalBtn: document.querySelector('[data-id="modal-btn"]'),
+//     turn: document.querySelector('[data-id="turn"]'),
+//   },
 
-    init() {
-        App.registerEventListeners()
+//   state: {
+//     currentPlayer: 1,
+//     moves: [],
+//   },
 
-    },
+//   getGameStatus(moves) {
+//     const p1moves = moves
+//       .filter((move) => move.playerId === 1)
+//       .map((move) => +move.squareId);
+//     const p2moves = moves
+//       .filter((move) => move.playerId === 2)
+//       .map((move) => +move.squareId);
 
-    registerEventListeners() {
-        App.$.menu.addEventListener('click', event => {
-            App.$.menuItems.classList.toggle('hidden')
-        });
+//     console.log(p1moves);
 
-        App.$.resetBtn.addEventListener('click', event => {
-            console.log('Reset the game')
-        });
+//     const winningPatterns = [
+//       [1, 2, 3],
+//       [1, 5, 9],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [3, 5, 7],
+//       [3, 6, 9],
+//       [4, 5, 6],
+//       [7, 8, 9],
+//     ];
 
-        App.$.newRoundBtn.addEventListener('click', event => {
-            console.log('Add a new round')
-        });
+//     let winner = null;
+//     winningPatterns.forEach((pattern) => {
+//       const p1Wins = pattern.every((v) => p1moves.includes(v));
+//       const p2Wins = pattern.every((v) => p2moves.includes(v));
 
-        App.$.squares.forEach((square) => {
-            square.addEventListener('click', (event) => {
-                console.log(`Square with id ${event.target.id} was clicked`)
-                console.log( `Current player is ${App.state.currentPlayer}`)
+//       console.log;
 
-                const currentPlayer = App.state.currentPlayer
+//       if (p1Wins) winner = 1;
+//       if (p2Wins) winner = 2;
+//     });
 
-                const icon = document.createElement('i')
+//     return {
+//       status: moves.length === 9 || winner != null ? "complete" : "in progress",
+//       winner,
+//     };
+//   },
 
+//   init() {
+//     App.registerEventListeners();
+//   },
 
-                if (currentPlayer === 1) {
-                    icon.classList.add('fa-solid', 'fa-x', 'yellow')
-                } else {
-                    icon.classList.add('fa-solid', 'fa-o', 'turquoise')
-                }
+//   registerEventListeners() {
+//     App.$.menu.addEventListener("click", (event) => {
+//         App.$.menuItems.classList.toggle("hidden");
+//     });
 
-                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1
+//     App.$.resetBtn.addEventListener("click", (event) => {
+//       console.log("Reset the game");
+//     });
 
-                event.target.replaceChildren(icon)
-            })
-        })
-    },
+//     App.$.newRoundBtn.addEventListener("click", (event) => {
+//       console.log("Add a new round");
+//     });
+
+//     App.$.modalBtn.addEventListener('click', event => {
+//         App.state.moves = []
+//         App.$.squares.forEach((square) => square.replaceChildren())
+//         App.$.modal.classList.add('hidden')
+//     })
+
+//     App.$.squares.forEach((square) => {
+//       square.addEventListener("click", (event) => {
+//         //Check if there is already a play, if so, return early
+//         const hasMove = (squareId) => {
+//           const existingMove = App.state.moves.find(
+//             (move) => move.squareId === squareId
+//           );
+//           return existingMove !== undefined;
+//         };
+
+//         if (hasMove(+square.id)) {
+//           return;
+//         }
+
+//         //Determine which player icon to add to square
+//         const lastMove = App.state.moves.at(-1);
+//         const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+//         const currentPlayer =
+//           App.state.moves.length === 0
+//             ? 1
+//             : getOppositePlayer(lastMove.playerId);
+//         const nextPlayer = getOppositePlayer(currentPlayer)
+
+//         const squareIcon = document.createElement("i");
+//         const turnIcon = document.createElement("i");
+//         const turnLabel = document.createElement('p')
+//         turnLabel.innerText = `Player ${nextPlayer}, you are up!`
+
+//         if (currentPlayer === 1) {
+//           squareIcon.classList.add("fa-solid", "fa-x", "yellow");
+//           turnIcon.classList.add("fa-solid", "fa-o", "turquoise");
+//           turnLabel.classList = 'turquoise'
+//         } else {
+//           squareIcon.classList.add("fa-solid", "fa-o", "turquoise");
+//           turnIcon.classList.add("fa-solid", "fa-x", "yellow");
+//           turnLabel.classList = 'yellow'
+//         }
+
+//         App.$.turn.replaceChildren(turnIcon, turnLabel)
+
+//         App.state.moves.push({
+//           squareId: +square.id,
+//           playerId: currentPlayer,
+//         });
+
+//         square.replaceChildren(squareIcon);
+
+//         //Check if there is a win or tie game
+
+//         const game = App.getGameStatus(App.state.moves);
+
+//         if (game.status === "complete") {
+//           App.$.modal.classList.remove("hidden");
+
+//           let message = "";
+//           if (game.winner) {
+//             message = `Player ${game.winner} wins!`;
+//           } else {
+//             message = "Tie game!";
+//           }
+
+//           App.$.modalText.textContent = message;
+//         }
+//       });
+//     });
+//   },
+// };
+
+// window.addEventListener("load", App.init);
+
+function init() {
+    const view = new View()
+
+    view.bindGameResetEvent(event => {
+        console.log('Reset event')
+        console.log(event)
+    })
+
+    view.bindNewRoundEvent(event => {
+        console.log('New Round event')
+        console.log(event)
+    })
+
+    view.bindPlayerMoveEvent(event => {
+        console.log('Player Move event')
+        console.log(event)
+    })
+
+    console.log(view.$.turn)
 }
 
-window.addEventListener('load', App.init)
+window.addEventListener('load', init)
---
+++ vanilla/js/view.js
@@ -0,0 +1,76 @@
+export default class View {
+  $ = {};
+  $$ = {}
+
+  constructor() {
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]')
+    this.$.menuItems = this.#qs('[data-id="menu-popover"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    //UI-only event listeners
+    this.$.menuBtn.addEventListener('click', (event) => {
+        this.#toggleMenu()
+    }) 
+  }
+
+
+  //Register all the event listeners
+
+
+  bindGameResetEvent(handler) {
+      this.$.resetBtn.addEventListener('click', handler)
+  }
+
+  bindNewRoundEvent(handler) {
+    this.$.newRoundBtn.addEventListener("click", handler);
+
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.$$.squares.forEach((square) => {
+        square.addEventListener('click', handler)
+    })
+  }
+
+  //DOM helper mehtods
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle('hidden')
+    this.$.menuBtn.classList.toggle('border')
+
+    const icon = this.$.menuBtn.querySelector('i')
+
+    icon.classList.toggle('fa-chevron-down')
+    icon.classList.toggle('fa-chevron-up')
+  }
+
+
+  /**
+   * The #qs and #qsAll methods are "safe selectors", meaning they
+   * _guarantee_ the elements we select exist in the DOM (otherwise throw an error)
+   */
+
+  #qs(selector, parent) {
+      const el = parent ? parent.querySelector(selector) : document.querySelector(selector)
+
+      if(!el) throw new Error ('Could not find element')
+
+      return el
+  }
+
+  #qsAll(selector) {
+      const elList = document.querySelectorAll(selector)
+
+      if(!elList) throw new Error('Could not find elements')
+
+      return elList
+  }
+}
